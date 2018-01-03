a558ea8b5bb410df4ca4cd58595a32b79684b9e3
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2018-01-03 18:24:25 -0200

    New Site

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ArquivosComponent } from './arquivos/arquivos.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArquivosComponent,
+    BlogComponent,
+    ProjetosComponent,
+    SobreComponent,
+    ContatosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
